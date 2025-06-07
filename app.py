95c3c2ee557dd0ad1642e9e701d5bb9a3a8f4e64
Piroenzo <enzopiro80@gmail.com>
2025-06-07 16:43:51 -0300

    Configuración para Render

--- app.py
+++ app.py
@@ -410,4 +410,5 @@ def enviar_contacto():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False) 
